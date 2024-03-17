7c76a18a711564a12e02da926493624e903d056e
heorh1 <heorhi44@gmail.com>
2024-03-18 00:51:15 +0100

    adding crud

--- FishPredictor/Controllers/FishController.cs
+++ FishPredictor/Controllers/FishController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FishPredictor.Controllers
 {
@@ -6,23 +7,74 @@ namespace FishPredictor.Controllers
     [Route("api/[controller]")]
     public class FishesController : ControllerBase
     {
+        private readonly FishContext _context;
+
+        public FishesController(FishContext context)
+        {
+            _context = context;
+        }
+
+        // GET ALL
         [HttpGet]
         public IActionResult GetAllFishes()
         {
-            var fishes = FishData.GetFishData();
+            var fishes = _context.Fishes.ToList();
             return Ok(fishes);
         }
 
-        [HttpGet("{name}")]
-        public IActionResult GetFishDetails(string name)
+        // GET
+        [HttpGet("{id}")]
+        public IActionResult GetFishDetails(int id)
         {
-            var fish = FishData.GetFishData().SingleOrDefault(f => f.Name.ToLower() == name.ToLower());
+            var fish = _context.Fishes.Find(id);
 
-            if (fish != null)
+            if (fish == null)
             {
-                return Ok(fish);
+                return NotFound();
             }
-            return NotFound();
+            return Ok(fish);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult CreateFish([FromBody] Fish fish)
+        {
+            _context.Fishes.Add(fish);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetFishDetails), new { id = fish.Id }, fish);
+        }
+
+        // PUT
+        [HttpPut("{id}")]
+        public IActionResult UpdateFish(int id, [FromBody] Fish fish)
+        {
+            if (id != fish.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fish).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFish(int id)
+        {
+            var fish = _context.Fishes.Find(id);
+
+            if (fish == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fishes.Remove(fish);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
--- FishPredictor/Startup.cs
+++ FishPredictor/Startup.cs
@@ -1,6 +1,21 @@
-﻿namespace FishPredictor
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace FishPredictor
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<FishContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("BloggingDatabase")));
+        }
     }
 }
